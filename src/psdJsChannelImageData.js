3c9dc1da1ecb7fc672b9c681bee6fa1267834fcf
Eric J. Duran <eric.duran7@gmail.com>
2012-09-22 03:59:04 -0400

    Figured out channelImageData parsing logic

--- src/psdJsChannelImageData.js
+++ src/psdJsChannelImageData.js
@@ -19,63 +19,65 @@ var psdJsChannelImageData = (function() {
     //      If the layer's size, and therefore the data, is odd, a pad byte will be inserted at the end of the row.
     //      If the layer is an adjustment layer, the channel data is undefined (probably all white.)
 
-    this.compression = psd.ds.readUint16();
+    this.rows = layerRecords[index].bottom - layerRecords[index].top;
+    this.cols = layerRecords[index].right - layerRecords[index].left;
+    this.rb = parseInt((this.cols * psd.header.depth + 7) / 8);
+    this.ch = [];
 
-    // If the compression code is 0, the image data is just the raw image data,
-    // whose size is calculated as (LayerBottom-LayerTop)* (LayerRight-LayerLeft)
-    // (from the first field in See Layer records).
-    if (this.compression === 0) {
-      this.dataSize = (layerRecords[index].bottom - layerRecords[index].top) * (layerRecords[index].right - layerRecords[index].left);
-      if (this.dataSize === 0) {
-        return;
-      }
-      else {
-        if (this.dataSize == 2000000) {
-          var elementSize = this.dataSize * Uint8Array.BYTES_PER_ELEMENT;
-          var buffer = new ArrayBuffer(elementSize);
-          this.data = new Uint8Array(buffer);
-          for (var i = 0; i < this.data.length; i++) {
-            this.data[i] = psd.ds.readUint8();
-          }
-          var tempcanvas = document.createElement('canvas');
-          tempcanvas.height = (layerRecords[index].bottom - layerRecords[index].top);
-          tempcanvas.width = (layerRecords[index].right - layerRecords[index].left);
-          var tempcontext = tempcanvas.getContext('2d');
-
-          var imgdata = tempcontext.getImageData(0,0,tempcanvas.width,tempcanvas.height);
-          console.log(this.data.length);
-          var imgdatalen = this.data.length;
-          for(var i=0; i<imgdatalen; i++)
-          {
-               imgdata.data[i] = this.data[i];
-          }
-          console.log(imgdata);
-          tempcontext.putImageData(imgdata,0,0);
-          var img = document.getElementById('img');
-                       img.height = tempcanvas.height;
-                       img.width = tempcanvas.width;
-                       img.src = tempcanvas.toDataURL();
-          console.log(tempcontext);
-          console.log(img);
-
-        }
-        else {
-          psd.ds.position = psd.ds.position + this.dataSize;
-        }
-      }
+    // This isn't really explain in the docs. Took a while to figure out.
+    // The image data is per channel, each channel has a lenght. You can see
+    // the channel in from the LayerRecords. We moved through the imageData
+    // by seeking accross the left of each channel data. This always
+    // means the raw image data size is not actually the size of bytes in this
+    // block. I haven't been able to find the image data yet. But this does
+    // parse all the encription correctly and seeks forward to the next expect
+    // location for now. Most of this was abstracted out from http://telegraphics.com.au/svn/psdparse/trunk/
+    // I build psdParse and ran the same psd to compare output. This lead me to this logic.
+    // This does break in certain PSDs. More testing required.
+    for (var i = 0; i < layerRecords[index].channels; i++) {
+      this.ch[i] = {};
+      this.ch[i].rowbytes  = this.rb;
+      this.ch[i].pos = psd.ds.position;
+      this.ch[i].rows = this.rows;
+      this.ch[i].cols = this.cols;
+      this.ch[i].compression = psd.ds.readUint16();
+      // We need to remove 2 for reading the compression.
+      psd.ds.position = psd.ds.position + (layerRecords[index].channelsInfo[i].len - 2);
     }
-    else {
-      if (this.compression == 1) {
-        // RLE
-        psd.ds.position = psd.ds.position + this.dataSize;
-        console.log(psd.ds.readUint8());
-      }
-      else {
-        psd.ds.position = psd.ds.position + this.dataSize;
-        console.log('Different compression');
-      }
-    }
+
+
+
+    // TODO: CONTINUE FROM HERE. THIS IS WHERE I LEFT OF LAST on 9/22/2012
+    // if (this.compression === 0) {
+    //   // for (var i = 0; i < 2; i++) {
+    //     if (this.rows) {
+    //       this.maybeSize = (this.rb * this.rows);
+    //       psd.ds.position = psd.ds.position + (this.rb * this.rows)  - 2x;
+    //     }
+    //   // }
+
+    // }
   }
+
+  psdJsChannelImageData.prototype = {
+
+    getDataSize: function(compression) {
+      if (compression === 0) {
+        // If the compression code is 0, the image data is just the raw image
+        // data, whose size is calculated as (LayerBottom-LayerTop)* (LayerRight-LayerLeft)
+        // (from the first field in See Layer records).
+      }
+      else if (compression === 1) {
+
+      }
+      else if (compression === 2) {
+
+      }
+      else if (compression === 3) {
+
+      }
+    }
+  };
 
   return psdJsChannelImageData;
 })();
